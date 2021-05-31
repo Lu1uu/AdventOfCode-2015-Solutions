d438742058b8eeadf83388e85b485ca522fe76d9
Lu1uu <hello.navi.98@gmail.com>
2021-05-30 17:16:47 -0700

    Day 2 Better, smarter solution

--- Day_2/Part_2.js
+++ Day_2/Part_2.js
@@ -8,10 +8,7 @@ const list = input.split('\n')
 let total = 0
 for (let dimensions of list) {
     let [l, w, h] = dimensions.split('x').map((num) => +num)
-    let maxValue = Math.max(l, w, h)
-    let [min1 = maxValue, min2 = maxValue] = [l, w, h].filter(
-        (value) => value != maxValue
-    )
+    let [min1, min2] = [l, w, h].sort((a, b) => a - b)
     const a = l * w * h
     const b = min1 * 2 + min2 * 2
     total += a + b
