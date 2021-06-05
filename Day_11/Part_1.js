f9889f29b40663a1caadb5723c687df976fea767
Lu1uu <hello.navi.98@gmail.com>
2021-06-05 05:21:48 -0700

    Almost done

---
+++ Day_11/Part_1.js
@@ -0,0 +1,57 @@
+let PASSWORD = 'abcdefgh'
+let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+
+const iterateLetter = (letter) => alphabet[(alphabet.indexOf(letter) + 1) % 26]
+const isLastLetter = (letter) => (letter == 'z' ? true : false)
+
+const newPassword = (password) => {
+    let passwordArr = [...password]
+    let index = passwordArr.length - 1
+    if (!isLastLetter(passwordArr[index])) {
+        passwordArr[index] = iterateLetter(passwordArr[index])
+    } else {
+        while (isLastLetter(passwordArr[index])) {
+            passwordArr[index] = iterateLetter(passwordArr[index--])
+            passwordArr[index - 1] = iterateLetter(passwordArr[index - 1])
+        }
+    }
+    return passwordArr.join('')
+}
+
+const fetchNewPassword = (password) => {
+    while (!isValid(password)) {
+        console.log(password)
+        password = newPassword(password)
+    }
+    return password
+}
+
+const hasStraight = (password) => {
+    for (let i = 0; i < password.length - 2; i++) {
+        if (
+            alphabet.indexOf(password[i]) ===
+                alphabet.indexOf(password[i + 1]) - 1 &&
+            alphabet.indexOf(password[i + 2]) - 2 ===
+                alphabet.indexOf(password[i])
+        ) {
+            return true
+        }
+    }
+    return false
+}
+
+const hasNoForbbidens = (password) => {
+    const forbiddenLetters = ['i', 'o', 'l']
+    for (const letter of password) {
+        if (forbiddenLetters.includes(letter)) return false
+    }
+    return true
+}
+
+const hasUniquePair = (password) => /([a-z])\1.*([a-z])\2/.test(password)
+const isValid = (password) =>
+    hasStraight(password) &&
+    hasNoForbbidens(password) &&
+    hasUniquePair(password)
+
+console.log(fetchNewPassword(PASSWORD))
